0485563fcd06444d55ca41e03ae48eb6425080b9
Arnab Das <arnab@coderuse.com>
2017-10-16 17:33:40 +0530

    Added sync for Azure cloud blob storage

---
+++ SyncAzureBlob/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+
+namespace SyncAzureBlob
+{
+    /// <summary>
+    /// This class provides some worker functions for the specific need for this project
+    /// But these functions can also be used in other projects also, as they are generic in nature
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// This is an extension method for the actual get settings method
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>string</returns>
+        public static string GetSettings(this string key)
+        {
+            return Setting<string>(key);
+        }
+
+        /// <summary>
+        /// This method can be used as an extractor method to get a value against the supplied key
+        /// from the AppSettings of the config file in current context
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns>string</returns>
+        private static T Setting<T>(string key)
+        {
+            string value = ConfigurationManager.AppSettings[key];
+
+            if (value == null)
+            {
+                throw new Exception(String.Format("Could not find '{0}' in app settings", key));
+            }
+
+            return (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ SyncAzureBlob/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Threading.Tasks;
+
+namespace SyncAzureBlob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string storeToPath = "StoreIn".GetSettings();
+            string[] runInterval = "RunInterval".GetSettings().Split(':');
+            while(true)
+            {
+                ProcessBlobFiles(storeToPath).Wait();
+                Task.Delay(new TimeSpan(int.Parse(runInterval[0]), int.Parse(runInterval[1]), int.Parse(runInterval[2]), int.Parse(runInterval[3]))).Wait();
+            }
+        }
+
+        static async Task ProcessBlobFiles(string storeToPath)
+        {
+            try
+            {
+                // Parse the connection string and return a reference to the storage account.
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                    CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+                // Create a blob client
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+                // Retrieve a reference to a container.
+                CloudBlobContainer container = blobClient.GetContainerReference("logs");
+
+                // Probably this is not required
+                //container.CreateIfNotExists();
+
+                // Loop over items within the container and output the length and URI.
+                foreach (IListBlobItem item in container.ListBlobs(null, false))
+                {
+                    if (item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blob = (CloudBlockBlob)item;
+
+                        Console.WriteLine("Downloding: '{0}'", blob.Name);
+
+                        // Retrieve reference to a blob named "photo1.jpg".
+                        CloudBlockBlob blockBlob = container.GetBlockBlobReference(blob.Name);
+
+                        // Save blob contents to a file.
+                        string toSave = storeToPath + blob.Name;
+                        using (var fileStream = System.IO.File.OpenWrite(toSave))
+                        {
+                            await blockBlob.DownloadToStreamAsync(fileStream);
+                        }
+                        Console.WriteLine("Download completed for: {0}", blob.Name);
+                        await blob.DeleteAsync();
+                        Console.WriteLine("Download completed for: {0}", blob.Name);
+                    }
+                    else if (item.GetType() == typeof(CloudPageBlob))
+                    {
+                        CloudPageBlob pageBlob = (CloudPageBlob)item;
+
+                        Console.WriteLine("Page blob of length {0}: {1}", pageBlob.Properties.Length, pageBlob.Uri);
+
+                    }
+                    else if (item.GetType() == typeof(CloudBlobDirectory))
+                    {
+                        CloudBlobDirectory directory = (CloudBlobDirectory)item;
+
+                        Console.WriteLine("Directory: {0}", directory.Uri);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Encountered error: {0}", ex.Message);
+            }
+        }
+    }
+}
